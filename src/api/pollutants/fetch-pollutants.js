550ab3d83fe83393da084d40dad04e9dfab57e8d
ulysses <ulysses.alvarez@cognizant.com>
2024-03-29 16:23:51 +0000

    fixed proxyFetch issues.

--- src/api/pollutants/fetch-pollutants.js
+++ src/api/pollutants/fetch-pollutants.js
@@ -5,52 +5,52 @@ import { proxyFetch } from '~/src/helpers/proxy-fetch'
 import { createLogger } from '~/src/helpers/logging/logger'
 import { pollutantUpdater } from '~/src/api/pollutants/helpers/pollutants-updater'
 import { config } from '~/src/config'
-import moment from 'moment'; 
+import moment from 'moment';
 
 const logger = createLogger()
 
 const fetchPollutants = async () => {
   const url = config.get('pollutantstUrl')
-  
+
   const res3 = await proxyFetch(url + 3);
-  const NorthEastScotland = await res3.json();
+  const northEastScotlandJSON = await res3.json();
   const res4 = await proxyFetch(url + 4);
-  const NorthWales = await res4.json();
+  const northWalesJSON = await res4.json();
   const res5 = await proxyFetch(url + 5);
-  const Highland = await res5.json();
+  const highlandJSON = await res5.json();
   const res6 = await proxyFetch(url + 6);
-  const CentralScotland = await res6.json();
+  const centralScotlandJSON = await res6.json();
   const res7 = await proxyFetch(url + 7);
-  const Eastern = await res7.json();
+  const easternJSON = await res7.json();
   const res8 = await proxyFetch(url + 8);
-  const SouthEast = await res8.json();
+  const southEastJSON = await res8.json();
   const res9 = await proxyFetch(url + 9);
-  const SouthWales = await res9.json();
+  const southWalesJSON = await res9.json();
   const res10 = await proxyFetch(url + 10);
-  const NorthWestAndMerseyside = await res10.json();
+  const northWestAndMerseysideJSON = await res10.json();
   const res11 = await proxyFetch(url + 11);
-  const SouthWest = await res11.json();
+  const southWestJSON = await res11.json();
   const res12 = await proxyFetch(url + 12);
-  const EastMidlands = await res12.json();
+  const eastMidlandsJSON = await res12.json();
   const res13 = await proxyFetch(url + 13);
-  const ScottishBorders = await res13.json();
+  const scottishBordersJSON = await res13.json();
   const res14 = await proxyFetch(url + 14);
-  const NorthEast = await res14.json();
+  const northEastJSON = await res14.json();
   const res15 = await proxyFetch(url + 15);
-  const GreaterLondon = await res15.json();
+  const greaterLondonJSON = await res15.json();
   const res16 = await proxyFetch(url + 16);
-  const WestMidlands = await res16.json();
+  const westMidlandsJSON = await res16.json();
   const res17 = await proxyFetch(url + 17);
-  const YorkshireAndHumberside = await res17.json();
+  const yorkshireAndHumbersideJSON = await res17.json();
   const res18 = await proxyFetch(url + 18);
-  const IsleofMan = await res18.json();
+  const isleofManJSON = await res18.json();
   console.log(moment().toISOString())
 
-  const result3 = NorthEastScotland.map(item => {
+  const northEastScotlandObj = northEastScotlandJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -64,11 +64,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result4 = NorthWales.map(item => {
+  const northWalesObj = northWalesJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -82,11 +82,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result5 = Highland.map(item => {
+  const highlandObj = highlandJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time:{date:""}}})
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -100,11 +100,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result6 = CentralScotland.map(item => {
+  const centralScotlandObj = centralScotlandJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -118,11 +118,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result7 = Eastern.map(item => {
+  const easternObj = easternJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -136,11 +136,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result8 = SouthEast.map(item => {
+  const southEastObj = southEastJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -154,11 +154,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result9 = SouthWales.map(item => {
+  const southWalesObj = southWalesJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -172,11 +172,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result10 = NorthWestAndMerseyside.map(item => {
+  const northWestAndMerseysideObj = northWestAndMerseysideJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -190,11 +190,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result11 = SouthWest.map(item => {
+  const southWestObj = southWestJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -208,11 +208,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result12 = EastMidlands.map(item => {
+  const eastMidlandsObj = eastMidlandsJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -226,11 +226,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result13 = ScottishBorders.map(item => {
+  const scottishBordersObj = scottishBordersJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -244,11 +244,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result14 = NorthEast.map(item => {
+  const northEastObj = northEastJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -262,11 +262,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result15 = GreaterLondon.map(item => {
+  const greaterLondonObj = greaterLondonJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -280,11 +280,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result16 = WestMidlands.map(item => {
+  const westMidlandsObj = westMidlandsJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -298,11 +298,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result17 = YorkshireAndHumberside.map(item => {
+  const yorkshireAndHumbersideObj = yorkshireAndHumbersideJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -316,11 +316,11 @@ const fetchPollutants = async () => {
     }
   })
 
-  const result18 = IsleofMan.map(item => {
+  const isleofManObj = isleofManJSON.map(item => {
     let newObj = {}
     item.parameter_ids.forEach(el => {
       if (['O3', 'NO2', 'GE10', 'PM25', 'SO2'].includes(el.parameter_id)) {
-        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" } } })
+        newObj = Object.assign({}, newObj, { [el.parameter_id]: { featureOfInterest: el.feature_of_interest[0].featureOfInterset, time: { date: "" }, exception: "" } })
       }
     })
     return {
@@ -333,10 +333,18 @@ const fetchPollutants = async () => {
       pollutants: { ...newObj }
     }
   })
-  const results3 = await pollutantUpdater(result3)
-  const results4 = await pollutantUpdater(result4)
-  const results5 = await pollutantUpdater(result5)
-  // const results6 = await pollutantUpdater(result6)
+  // const NorthEastScotlandResults = await pollutantUpdater(NorthEastScotland)
+  // const results4 = await pollutantUpdater(result4)
+  // const results5 = await pollutantUpdater(result5)
+  //                                     const results6 = await pollutantUpdater(CentralScotland)
+  const centralScotlandObjSplit = centralScotlandObj.reduce((all, one, i) => {
+    const ch = Math.floor(i % 3);
+    all[ch] = [].concat((all[ch] || []), one);
+    return all
+  }, [])
+  const centralScotlandResults1 = await pollutantUpdater(centralScotlandObjSplit[0])
+  const centralScotlandResults2 = await pollutantUpdater(centralScotlandObjSplit[1])
+  const centralScotlandResults3 = await pollutantUpdater(centralScotlandObjSplit[2])
   // const results7 = await pollutantUpdater(result7)
   // const results8 = await pollutantUpdater(result8)
   // const results9 = await pollutantUpdater(result9)
@@ -349,12 +357,14 @@ const fetchPollutants = async () => {
   // const results16 = await pollutantUpdater(result16)
   // const results17 = await pollutantUpdater(result17)
   // const results18 = await pollutantUpdater(result18)
- 
+
   const measurements = [
-    ...results3,
-    ...results4,
-    ...results5
-    // ...results6,
+    // ...results3,
+    // ...results4,
+    // ...results5,
+    ...centralScotlandResults1,
+    ...centralScotlandResults2,
+    ...centralScotlandResults3
     // ...results7,
     // ...results8,
     // ...results9,
@@ -369,7 +379,7 @@ const fetchPollutants = async () => {
     // ...results18
   ]
   return measurements
-  
+
 }
 
 const savePollutants = async (server, pollutants) => {
--- src/api/pollutants/helpers/pollutants-updater.js
+++ src/api/pollutants/helpers/pollutants-updater.js
@@ -6,110 +6,119 @@ import reduce from 'await-reduce'
 import xmlToJs from 'xml-js'
 import { config } from '~/src/config'
 import { XMLParser, XMLBuilder } from 'fast-xml-parser'
+import moment from 'moment'
 
 const urlExtra = config.get('pollutantstUrlExtra')
-const startTimeStamp = '2023-12-13T23:00:00.000Z'
-const endTimeStamp = '2023-12-15T01:00:00.00'
+const startTimeStamp = moment().add(-1, 'days').set({ hour: 23, minute: 0, second: 0, millisecond: 0 }).toISOString()
+const endTimeStamp = moment().add(1, 'days').set({ hour: 0, minute: 0, second: 0, millisecond: 0 }).toISOString()
 const timestamp = `${startTimeStamp}/${endTimeStamp}`
-//
+
 const parser = new XMLParser()
 const builder = new XMLBuilder();
-export async function pollutantUpdater (data) {
+export async function pollutantUpdater(data) {
     let promises = []
-    data.forEach(async (site, index) => {
-      try {
-          const { pollutants } = site
-          // eslint-disable-next-line prefer-const
-          Object.entries(pollutants).map(async ([k, v], i) => {
-              
-              if (v.featureOfInterest !== 'missingFOI') {
-                  try {
-                      promises = [
-                          ...promises,
-                          {
-                              [k]: proxyFetch(
-                                  `${urlExtra}${timestamp}&featureOfInterest=${v.featureOfInterest}`
-                              )
-                          }
-                      ]
-                  } catch (error) {
-                      console.log(error)
-                  }
-              } else {
-                  promises = [
-                      ...promises,
-                      {
-                          [k]: Promise.resolve("missingFOI")
-                      }
-                  ]
-              }
-              })
+    data.forEach((site, index) => {
+        try {
+            const { pollutants } = site
+            // eslint-disable-next-line prefer-const
+            Object.entries(pollutants).forEach(([k, v], i) => {
+
+                if (v.featureOfInterest !== 'missingFOI') {
+                    try {
+                        promises = [
+                            ...promises,
+                            {
+                                [k]: proxyFetch(
+                                    `${urlExtra}${timestamp}&featureOfInterest=${v.featureOfInterest}`
+                                )
+                            }
+                        ]
+                    } catch (error) {
+                        console.log(error)
+                    }
+                } else {
+                    promises = [
+                        ...promises,
+                        {
+                            [k]: Promise.resolve("missingFOI")
+                        }
+                    ]
+                }
+            })
         } catch (error) {
-        console.log(error)
+            console.log(error)
         }
-  })
-    const promisesOnly = promises.map((item) => { //
-    return Object.entries(item)[0][1]
-  })
+    })
+    const promisesOnly = promises.map((item) => {
+        return Object.entries(item)[0][1]
+    })
     let valueMeasured = ''
     let dateMeasured = ''
     let tempDate = []
-  const results = await reduce(
-    promisesOnly,
-      async (accumulator, response, index, array) => {
-          if (!response.ok && response !== "missingFOI") {
-              return accumulator
-          }
-          try {
-              if (response === "missingFOI") {
-                  valueMeasured = 0
-                  dateMeasured = ""
-              } else {
-                  const body = parser.parse(await response.text())
-                  const xmlContent = builder.build(body)
-                  let jsonResult = xmlToJs.xml2json(xmlContent, {
-                      compact: true,
-                      spaces: 4
-                  })
-                  jsonResult = JSON.parse(jsonResult)
-                  if (jsonResult) {
-                      valueMeasured = jsonResult?.['gml:FeatureCollection']?.['gml:featureMember'][1]?.[
-                          'om:OM_Observation'
-                      ]?.['om:result']?.['swe:DataArray']?.['swe:values']._text
-                          .split(',')
-                          .pop()
-                      tempDate = jsonResult?.['gml:FeatureCollection']?.['gml:featureMember'][1]?.[
-                          'om:OM_Observation'
-                      ]?.['om:result']?.['swe:DataArray']?.['swe:values']._text
-                          .split(',')
-                      dateMeasured = tempDate[tempDate.length - 4]
-                  }
-              }
+    let exceptionReport = ""
+    const results = await reduce(
+        promisesOnly,
+        async (accumulator, response, index, array) => {
+            if (!response.ok && response !== "missingFOI") {
+                return accumulator
+            }
+            try {
+                if (response === "missingFOI") {
+                    valueMeasured = 0
+                    dateMeasured = ""
+                } else {
+                    const body = parser.parse(await response.text())
+                    const xmlContent = builder.build(body)
+                    let jsonResult = xmlToJs.xml2json(xmlContent, {
+                        compact: true,
+                        spaces: 4
+                    })
+                    jsonResult = JSON.parse(jsonResult)
+                    if (jsonResult && !jsonResult?.['ows:ExceptionReport']) {
+                        valueMeasured = jsonResult?.['gml:FeatureCollection']?.['gml:featureMember'][1]?.[
+                            'om:OM_Observation'
+                        ]?.['om:result']?.['swe:DataArray']?.['swe:values']._text
+                            .split(',')
+                            .pop()
+                        tempDate = jsonResult?.['gml:FeatureCollection']?.['gml:featureMember'][1]?.[
+                            'om:OM_Observation'
+                        ]?.['om:result']?.['swe:DataArray']?.['swe:values']._text
+                            .split(',')
+                        dateMeasured = tempDate[tempDate.length - 4]
+                        exceptionReport = ''
+                    }
+                      if (jsonResult?.['ows:ExceptionReport']) {
+                          valueMeasured = 'Error while querying observation data!'
+                          exceptionReport = 'Error while querying observation data!'
+                          dateMeasured = 0
+                      }
+                }
 
-          } catch (error) {
-              console.log(error)
-          }
+            } catch (error) {
+                console.log(error)
+            }
 
-          return [...accumulator, { value: valueMeasured, time: { date: dateMeasured } }]
-    },
-    []
-  )
-  let measuredIndex = 0
-  const insertPollutantsValues = (results) => {
-    data.forEach((site, index) => {
-      try {
-        const { pollutants } = site
-        // eslint-disable-next-line prefer-const
-        Object.entries(pollutants).forEach(([k, v], i) => {
-            pollutants[k].value = results[measuredIndex]?.value ? Math.round(results[measuredIndex]?.value) : 0
-            pollutants[k].time.date = results[measuredIndex]?.value ? results[measuredIndex]?.time.date : 0
-            measuredIndex++
+            return [...accumulator, { exception: exceptionReport, value: valueMeasured, time: { date: dateMeasured } }]
+        },
+        []
+    )
+    let measuredIndex = 0
+    const insertPollutantsValues = (results) => {
+        data.forEach((site, index) => {
+            try {
+                const { pollutants } = site
+                // eslint-disable-next-line prefer-const
+                Object.entries(pollutants).forEach(([k, v], i) => {
+                    pollutants[k].value = results[measuredIndex]?.value ? Math.round(results[measuredIndex]?.value) : 0
+                    pollutants[k].time.date = results[measuredIndex]?.value ? results[measuredIndex]?.time.date : 0
+                    pollutants[k].exception = results[measuredIndex]?.exception
+                    measuredIndex++
+                })
+            } catch (error) {
+                console.log(error)
+            }
         })
-      } catch (error) {
-        console.log(error)
-      }
-    })
-  }
+    }
     insertPollutantsValues(results)
     return data
 }
