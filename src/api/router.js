5bd6c1ba8cba14138cae439fee2dff859507c19d
ulysses <ulysses.alvarez@cognizant.com>
2024-04-04 12:48:53 +0100

    added route path for historical data.

--- src/api/router.js
+++ src/api/router.js
@@ -1,13 +1,16 @@
-import { health } from '~/src/api/health'
-import { example } from '~/src/api/example'
-import { forecast } from '~/src/api/forecast'
-import { measurements } from '~/src/api/pollutants'
+import { forecast, historicalForecasts } from '~/src/api/forecast'
+import { measurements, historicalMeasurements } from '~/src/api/pollutants'
 
 const router = {
   plugin: {
     name: 'Router',
     register: async (server) => {
-      await server.register([health, example, forecast, measurements])
+      await server.register([
+        forecast,
+        historicalForecasts,
+        measurements,
+        historicalMeasurements
+      ])
     }
   }
 }
