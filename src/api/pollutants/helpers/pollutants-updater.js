6cc2a47f202dd563509fa051c48b73972debf033
ulysses <ulysses.alvarez@cognizant.com>
2024-05-06 19:17:16 +0100

    created variables for logs values.

--- src/api/pollutants/helpers/pollutants-updater.js
+++ src/api/pollutants/helpers/pollutants-updater.js
@@ -43,15 +43,17 @@ export async function pollutantUpdater(data) {
   const timestamp = `${startTimeStamp}/${endTimeStamp}`
 
   data.forEach((site, index) => {
+    const momentUTCFormat = moment.utc().format()
+    const momentVar = moment()
+    const momentBrackets = moment().utc().format()
+    const momentValueOf = moment().utc().toISOString().valueOf()
+    const momentTZ = moment().tz('Europe/London')
     logger.info(`timestamp for API call: ${timestamp}`)
-    logger.info('moment(): ', moment())
-    logger.info('moment.utc().format(): ', moment.utc().format())
-    logger.info('moment().utc().format(): ', moment().utc().format())
-    logger.info(
-      'moment().utc().toISOString().valueOf(): ',
-      moment().utc().toISOString().valueOf()
-    )
-    logger.info('moment().tz(Europe/London): ', moment().tz('Europe/London'))
+    logger.info('moment(): ', momentVar)
+    logger.info('moment.utc().format(): ', momentUTCFormat)
+    logger.info('moment().utc().format(): ', momentBrackets)
+    logger.info('moment().utc().toISOString().valueOf(): ', momentValueOf)
+    logger.info('moment().tz(Europe/London): ', momentTZ)
     try {
       const { pollutants } = site
       // eslint-disable-next-line prefer-const
