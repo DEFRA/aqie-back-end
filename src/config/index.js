03642089b7ebbd74c4c766974bbef5ad84c3de2b
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2025-09-03 10:57:54 +0100

    perf-test password fix 4.

--- src/config/index.js
+++ src/config/index.js
@@ -284,7 +284,7 @@ const config = convict({
     env: 'RICARDO_API_PASSWORD'
   },
   mockInvalidPollutants: {
-    doc: 'Enable mocking of invalid pollutant values (-9999, -99, 0) for testing',
+    doc: 'Enable mocking of invalid pollutant values (-9999, -99, 0) for testing pollutants with issues',
     format: Boolean,
     default: false,
     env: 'MOCK_INVALID_POLLUTANTS'
